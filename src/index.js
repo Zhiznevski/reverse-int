d7ecc2de78363717373bb9e0a7b7821883046ccb
Zhiznevski <w.heisenberggg@gmail.com>
2022-12-02 10:48:00 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let rev;
+    rev = n.toString().split("").reverse();
+    const newArray = rev.filter((element) => element !== "-");
+    return newArray.join("");
+};
